848f6b50fe11e3e66d3a1d3a295694782c19aee2
Gaurav <gauravgoswami967@gmail.com>
2026-04-10 23:38:58 +0530

    testing config

--- Testing/Experiment-6/vite.config.js
+++ Testing/Experiment-6/vite.config.js
@@ -14,6 +14,10 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    // setupFiles: './src/setupTests.js'
+    server: {
+      deps: {
+        inline: ['@exodus/bytes', 'html-encoding-sniffer']
+      }
+    }
   }
 })
